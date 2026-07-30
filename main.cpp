9cd7a111900e9e0b03f000fd2d07c342a9762540
chopinpiano0818-coder <chopinpiano0818@gmail.com>
2026-07-30 11:19:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include "Game.h"
 
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
 
-    SDL_SetMainReady();
     return runGame();
 }
